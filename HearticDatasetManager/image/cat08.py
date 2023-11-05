e694c1aedb0366336ab9bf2514240b73da36ef61
Matteoleccardi <leccardi.matteo@gmail.com>
2023-11-05 17:00:41 +0100

    cato8 started

--- HearticDatasetManager/image/cat08.py
+++ HearticDatasetManager/image/cat08.py
@@ -1,3 +1,13 @@
 """Utilities to import a cat08 dataset image into the base image class.
 """
+import os
+import numpy
+import SimpleITK
 
+from HearticDatasetManager.image.image import ImageCT
+
+
+def load_cat08_image() -> ImageCT:
+    pass
+
+# maybe a class is better...
