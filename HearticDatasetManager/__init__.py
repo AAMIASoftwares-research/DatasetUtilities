27df22c4300dea9f7f3bc201be5102383f0c83e7
Matteoleccardi <leccardi.matteo@gmail.com>
2023-11-07 11:20:15 +0100

    Init main module

---
+++ HearticDatasetManager/__init__.py
@@ -0,0 +1,20 @@
+""" Heartic Dataset Manager
+
+This module helps working with the data available to the HEARTIC project
+from the AAMIA Softwares research group.
+
+Available datasets are:
+
+* cat08
+* asoca
+
+while other datasets we are working on are:
+
+* molinette
+* monzino
+* deepvesselnet
+
+"""
+
+from . import cat08, asoca
+__all__ = ['cat08', 'asoca']
