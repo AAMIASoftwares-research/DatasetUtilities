80834f8b0939dc5d1a4d925c53e0e4cfff1799c6
Matteo Leccardi <leccardi.matteo@gmail.com>
2024-06-06 12:13:48 +0200

    added lumen segmentation with threshold to ImageCAS

---
+++ HearticDatasetManager/imagecas/automatic_lumen_thresholding/__main__.py
@@ -0,0 +1,112 @@
+import os
+import numpy
+from scipy.ndimage import binary_erosion
+import matplotlib.pyplot as plt
+import multiprocessing
+import nibabel
+
+from ..image import ImagecasImageCT, ImagecasLabelCT
+from ..dataset import DATASET_IMAGECAS_IMAGES, DATASET_IMAGECAS_LABELS
+
+BASE_FOLDER = "E:\\MatteoLeccardi\\HearticData\\ImageCAS\\"
+DATA_FOLDER = "Data\\"
+TARGET_FOLDER = "LabelsAugmentedLumenWall\\"
+
+NULL_LABEL = 0
+WALL_LABEL = 1
+LUMEN_LABEL = 2
+
+
+from .functions_for_multiprocessing import get_histogram
+if __name__ == "__main__" and 0:
+    # load all images, save intensities of pixels that are part of the label
+    # segmentations into a vector, at the end show the histogram
+    # do it with multiprocessing
+    N_CPU = multiprocessing.cpu_count()
+    with multiprocessing.Pool(N_CPU-1) as pool:
+        results = pool.starmap(
+            get_histogram, 
+            [
+                (BASE_FOLDER + DATA_FOLDER + im_, BASE_FOLDER + DATA_FOLDER + la_)
+                for i, (im_, la_) in enumerate(
+                    zip(DATASET_IMAGECAS_IMAGES, DATASET_IMAGECAS_LABELS)
+                )
+                #if i < 300
+            ]
+        )
+    results = numpy.array(results)
+    results = numpy.sum(results, axis=0)
+    pixel_intensities_histogram = results / numpy.sum(results)
+    plt.plot(range(-1000, 2001), pixel_intensities_histogram, "-", linewidth=0.6)
+    plt.show()
+
+
+# now create the labels for the lumen and wall
+# we start from the wall, as the label we already have is quite large and also includes arterial wall
+# then, we find the lumen by thresholding the image in the locations inside the wall label
+# threshold:
+
+from .functions_for_multiprocessing import make_wall_lumen_label
+
+LUMEN_THRESH = 150
+
+# test
+if __name__ == "__main__" and 0:
+    image_path = BASE_FOLDER + DATA_FOLDER + DATASET_IMAGECAS_IMAGES[0]
+    label_path = BASE_FOLDER + DATA_FOLDER + DATASET_IMAGECAS_LABELS[0]
+    new_label = make_wall_lumen_label(image_path, label_path, "", LUMEN_THRESH, LUMEN_LABEL, WALL_LABEL, NULL_LABEL)
+    
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    for i in range(20, 180):
+        ax.clear()
+        ax.imshow(new_label.data[:, :, i], vmin=0, vmax=2, cmap="coolwarm")
+        ax.set_xlabel(f"Slice {i}")
+        plt.pause(0.05)
+    plt.show()
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    image = ImagecasImageCT(image_path)
+    ax.imshow(image.data[:, :, 100], vmin=-1000, vmax=800, cmap="gray")
+    im_wall = numpy.zeros((image.data[:, :, 100].shape[0], image.data[:, :, 100].shape[1], 3), dtype=int)
+    im_wall[:,:,1] = (new_label.data[:, :, 100] == WALL_LABEL).astype(int)*255
+    ax.imshow(im_wall, alpha=0.1)
+    im_lumen = numpy.zeros((image.data[:, :, 100].shape[0], image.data[:, :, 100].shape[1], 3), dtype=int)
+    im_lumen[:,:,0] = (new_label.data[:, :, 100] == LUMEN_LABEL).astype(int)*255
+    ax.imshow(im_lumen, alpha=0.1)
+    plt.show()
+    
+    # test I/O
+    if 0:
+        if not os.path.exists(BASE_FOLDER + TARGET_FOLDER):
+            os.makedirs(BASE_FOLDER + TARGET_FOLDER)
+        new_label_path = BASE_FOLDER + TARGET_FOLDER + DATASET_IMAGECAS_LABELS[0]
+        make_wall_lumen_label(image_path, label_path, new_label_path)
+
+
+
+# MAIN
+
+if __name__ == "__main__" and 1:
+    import time
+    t0_ = time.time()
+    N_CPU = multiprocessing.cpu_count()
+    input_list = [
+        (
+            BASE_FOLDER + DATA_FOLDER + im_, 
+            BASE_FOLDER + DATA_FOLDER + la_, 
+            BASE_FOLDER + TARGET_FOLDER + la_, 
+            LUMEN_THRESH,
+            LUMEN_LABEL,
+            WALL_LABEL,
+            NULL_LABEL
+        )
+        for i, (im_, la_) in enumerate(
+            zip(DATASET_IMAGECAS_IMAGES, DATASET_IMAGECAS_LABELS)
+        )
+    ]
+    with multiprocessing.Pool(N_CPU-1) as pool:
+        pool.starmap(make_wall_lumen_label, input_list)
+    te = time.time()
+    print(f"Done in {te-t0_} seconds! (approx {(te-t0_)/60} minutes)")
+   
---
+++ HearticDatasetManager/imagecas/automatic_lumen_thresholding/functions_for_multiprocessing.py
@@ -0,0 +1,62 @@
+import numpy
+from ..image import ImagecasImageCT, ImagecasLabelCT
+from scipy.ndimage import binary_erosion
+import nibabel
+
+def get_histogram(image_file_path, label_file_path) -> list[float]:
+        image = ImagecasImageCT(image_file_path)
+        label = ImagecasLabelCT(label_file_path)
+        pixel_intensities_histogram = [0]*3001 # from -1000 to 2000 included
+        where = numpy.argwhere(label.data > 0)
+        for x, y, z in where:
+            if (image.data[x, y, z] >= -1000) and (image.data[x, y, z] <= 2000):
+                pixel_intensities_histogram[image.data[x, y, z]+1000] += 1
+        return pixel_intensities_histogram
+
+
+
+def make_wall_lumen_label(image_file_path, label_file_path, save_path, lumen_thresh=150, lumen_label=2, wall_label=1, null_label=0):
+    image = ImagecasImageCT(image_file_path)
+    label = ImagecasLabelCT(label_file_path)
+    # create the new label file, that has 0 and 1 as labels for Null and Wall
+    new_label = ImagecasLabelCT(label_file_path)
+    # outer layer erosion
+    new_label.data = binary_erosion(label.data.astype(bool), iterations=1)
+    new_label.data = new_label.data.astype(int)
+    new_label.data += label.data
+    # now, erode the lumen label so that a pixel does not have neighbours of Null label
+    # in the slice
+    where = numpy.argwhere(new_label.data == lumen_label)
+    for x, y, z in where:
+        if null_label in new_label.data[x-1:x+2, y-1:y+2, z]:
+            new_label.data[x, y, z] = wall_label
+    # intensity-based erosion of the wall label
+    # only in the locations where the previously found lumen is
+    # after finding the pixels we have to search,
+    # the lumen label is reset to be all just wall label
+    where = numpy.argwhere(new_label.data == lumen_label)
+    new_label.data = new_label.data.astype(bool).astype(int)
+    for x, y, z in where:
+        if image.data[x, y, z] >= lumen_thresh:
+            new_label.data[x, y, z] = lumen_label
+    # if a wessel wall pixel is surrounded, on the slice, 
+    # by lumen pixels, it is a lumen pixel
+    # this is to prevent holes in the lumen label
+    where = numpy.argwhere(new_label.data == wall_label)
+    for x, y, z in where:
+        if wall_label not in numpy.append(
+            [new_label.data[x-1:x+2, y-1, z].flatten(), 
+             new_label.data[x-1:x+2, y+1, z].flatten(), 
+             new_label.data[x-1, y, z].flatten(),
+             new_label.data[x+1, y, z].flatten()
+            ]):
+            new_label.data[x, y, z] = lumen_label
+    # return or save with nibabel to nii.gz
+    if save_path == "":
+        return new_label
+    nib_label = nibabel.load(label_file_path)
+    nib_label_new = nibabel.Nifti1Image(
+        numpy.flip(new_label.data, axis=0).astype(numpy.uint8),
+        nib_label.affine
+    )
+    nibabel.save(nib_label_new, save_path)
