4bc07921259e5036b7e444b939874fb186926ef6
Matteoleccardi <leccardi.matteo@gmail.com>
2025-07-01 12:39:07 +0200

    MAJOR: Asoca images are no longer able to open with SimpleITK, so had to implement opening with pynrrd

--- HearticDatasetManager/asoca/image.py
+++ HearticDatasetManager/asoca/image.py
@@ -3,6 +3,7 @@
 import os
 import numpy
 import SimpleITK
+import nrrd
 
 from HearticDatasetManager.affine import apply_affine_3d, compose_affines
 from HearticDatasetManager.image.image import ImageCT
@@ -55,36 +56,77 @@ class AsocaImageCT(ImageCT):
     def _load_image(self, path) -> None:
         """Load the image into the class.
         """
-        # Load the image
-        image = SimpleITK.ReadImage(
-            path,
-            SimpleITK.sitkInt16    
-        )
-        # Get the image array
-        image_array = SimpleITK.GetArrayFromImage(image)
-        # - While the Hounsfield units are fine for ASOCA images,
-        #   values can go down even to -3000. We could leave it like so,
-        #   but to give continuity with the other datasets for which the minimum is -1024,
-        #   (approx the HU of air), we clip all values below -1024 to -1024.
-        image_array[image_array < -1024] = -1024
-        # - now, data i,j,k correspond to z or S, y or A, x or R
-        # - transpose data so that you have data[i, j, k] where i->x or R, j->y or A, k->z or S
-        image_array = numpy.transpose(image_array, axes=(2, 1, 0))
-        # Get the image spacing
-        image_spacing = numpy.array(image.GetSpacing()).astype(numpy.float32).reshape((3,))
-        # Get the image origin
-        image_origin = numpy.array(image.GetOrigin()).astype(numpy.float32).reshape((3,))
-        # Get the image direction
-        # in ASOCA, the image direction provided by SimpleITK is always the identity matrix
-        # so it is basically useless. Keep the code for later reference.
-        # >> image_direction = numpy.array(image.GetDirection())
-        # In Slicer, to view the image in RAS coordinates, the following direction transform
-        # is applied to the image:
-        affine_ijk2ras_direction = numpy.eye(4)
-        affine_ijk2ras_direction[0,0] = -1.0
-        affine_ijk2ras_direction[1,1] = -1.0
-        # transform origin according to ras orientation
-        image_origin = apply_affine_3d(affine_ijk2ras_direction, image_origin)
+        try:
+            # Load the image - first try with SimpleITK
+            image = SimpleITK.ReadImage(
+                path,
+                SimpleITK.sitkInt16    
+            )
+            # Get the image array
+            image_array = SimpleITK.GetArrayFromImage(image)
+            # - While the Hounsfield units are fine for ASOCA images,
+            #   values can go down even to -3000. We could leave it like so,
+            #   but to give continuity with the other datasets for which the minimum is -1024,
+            #   (approx the HU of air), we clip all values below -1024 to -1024.
+            image_array[image_array < -1024] = -1024
+            # - now, data i,j,k correspond to z or S, y or A, x or R
+            # - transpose data so that you have data[i, j, k] where i->x or R, j->y or A, k->z or S
+            image_array = numpy.transpose(image_array, axes=(2, 1, 0))
+            # Get the image spacing
+            image_spacing = numpy.array(image.GetSpacing()).astype(numpy.float32).reshape((3,))
+            # Get the image origin
+            image_origin = numpy.array(image.GetOrigin()).astype(numpy.float32).reshape((3,))
+            # Get the image direction
+            # in ASOCA, the image direction provided by SimpleITK is always the identity matrix
+            # so it is basically useless. Keep the code for later reference.
+            # >> image_direction = numpy.array(image.GetDirection())
+            # In Slicer, to view the image in RAS coordinates, the following direction transform
+            # is applied to the image:
+            affine_ijk2ras_direction = numpy.eye(4)
+            affine_ijk2ras_direction[0,0] = -1.0
+            affine_ijk2ras_direction[1,1] = -1.0
+            # transform origin according to ras orientation
+            image_origin = apply_affine_3d(affine_ijk2ras_direction, image_origin)
+        except Exception as e:
+            # RECENTLY (JULY 2025) SIMPLE-ITK STARTED FAILING TO OPEN NRRD IMAGES -> FALL TO PYNRRD 
+            #
+            # Use pynrrd as fallback
+            image_array, header = nrrd.read(path)
+            # NRRD in this dataset is y, x, z; We do not transpose this, as all matrices works fine without transposition
+            # they break if the image is transposed.
+            # image_array = numpy.transpose(image_array, axes=(1, 0, 2))
+            # After transposition, data is actuually i -> L, j -> P, k -> S
+            # If we wanted the image to be with ijk compatible with ras, we could do
+            # the following, but then we would have to check that all matrices for conversions 
+            # (ijk2ras, etc...) work fine
+            #image_array = image_array[::-1, ::-1,:]
+            #
+            # Get spacing from header
+            if 'space directions' in header:
+                spacing = []
+                for v in header['space directions']:
+                    spacing.append(numpy.linalg.norm(v))
+                image_spacing = numpy.array(spacing, dtype=numpy.float32)
+            elif 'spacings' in header:
+                image_spacing = numpy.array(header['spacings'], dtype=numpy.float32)
+            else:
+                image_spacing = numpy.array([1.0, 1.0, 1.0], dtype=numpy.float32)
+            # Get origin from header
+            if 'space origin' in header:
+                image_origin = numpy.array(header['space origin'], dtype=numpy.float32)
+            else:
+                image_origin = numpy.zeros(3, dtype=numpy.float32)
+            # Get the image direction
+            # in ASOCA, the image direction provided by SimpleITK is always the identity matrix
+            # so it is basically useless. Keep the code for later reference.
+            # >> image_direction = numpy.array(image.GetDirection())
+            # In Slicer, to view the image in RAS coordinates, the following direction transform
+            # is applied to the image:
+            affine_ijk2ras_direction = numpy.eye(4)
+            affine_ijk2ras_direction[0,0] = -1.0
+            affine_ijk2ras_direction[1,1] = -1.0
+            # transform origin according to ras orientation
+            image_origin = apply_affine_3d(affine_ijk2ras_direction, image_origin)
         # out
         return (image_array, image_origin, image_spacing, affine_ijk2ras_direction)
     
